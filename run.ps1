73ac34206f192fc3862e7e5104dc2ad441c3dd91
Jose <jose.manuel.corral@gmail.com>
2023-01-01 19:20:04 +0100

    Delete run.ps1

--- run.ps1
+++
@@ -1,4 +0,0 @@
- $env:COSMOS_ENDPOINT = "https://indicosmos.documents.azure.com:443/"
- $env:COSMOS_KEY = "GVXCoCHCmISNSZwM7ldQhPwH7cwhNOmB3R46oXNKbeaeMRz6zLE3eCwKHyXwn5OwQ96D81xf1nfWACDb12lXAg=="
- 
- dotnet run --project .\code\Benchcosmoscli\Benchcosmoscli.csproj -c release
